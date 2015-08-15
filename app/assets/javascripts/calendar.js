cedc999388aac7dc5cea1fa603be26fa8944536c
Nishad Prinja <np327@cornell.edu>
2015-08-15 16:55:02 -0400

    working select/populate

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 
-
 	/* initialize the external events
 	-----------------------------------------------------------------*/
 
-	$('#external-events .fc-event').each(function() {
+$('#external-events .fc-event').each(function() {
 
 		// store data so the calendar knows to render an event upon drop
 		$(this).data('event', {
@@ -42,5 +41,47 @@ $(document).ready(function() {
 		}
 	});
 
+	$('#dept-select').on('change', function () {
+            var id = $(this).val();
+            var createDiv = '';
+            $.getJSON( '/deptcourses/' + id, function( data ) {
+
+            $.each(data, function () {
+                createDiv += '<div class="fc-event" data–courseid="' + this.id + '">'
+                createDiv += 'Course:' + this.course_name + '</div>'
+            });
+                $('#external-events').append(createDiv)
+
+	    $('#external-events .fc-event').each(function() {
+
+			// store data so the calendar knows to render an event upon drop
+			$(this).data('event', {
+				title: $.trim($(this).text()), // use the element's text as the event title
+				stick: true // maintain when user navigates (see docs on the renderEvent method)
+			});
+
+			// make the event draggable using jQuery UI
+			$(this).draggable({
+				zIndex: 999,
+				revert: true,      // will cause the event to go back to its
+				revertDuration: 0  //  original position after the drag
+			});
+
+		});
+            //Object {id: 1, name: "Drew Brees", handSizeInches: 10, created_at: "2015-08-03T15:02:10.751Z", updated_at: "2015-08-03T15:02:10.751Z"}
+
+            // For each item in our JSON, add a table row and cells to the content string
+            // $.each(data, function(){
+            //     tableContent += '<tr>';
+            //     tableContent += '<td>' + this.name + '</td>';
+            //     tableContent += '<td>' + this.handSizeInches + '</td>';
+            //     tableContent += '<td><a href="#" class="linkEditPlayer" data-id="' + this.id + '">edit</a></td>';
+            //     tableContent += '<td><a href="#" class="linkDeletePlayer" data-id="' + this.id + '">delete</a></td>';
+            //     tableContent += '</tr>';
+            // });
+
+            // Inject the whole content string into our existing HTML table
+        });
+    });
 
 });
